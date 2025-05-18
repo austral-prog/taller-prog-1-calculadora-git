f51a33e6ea35e1d82079acef6be5fc2b2eeb9855
Sebastian Iglesias <sebiglesias@users.noreply.github.com>
2025-05-18 14:33:58 -0300

    Create init.py

---
+++ operaciones/init.py
@@ -0,0 +1,6 @@
+# Exporta las funciones para importación directa
+from .basicas import suma
+from .avanzadas import potencia
+from .especiales import factorial
+
+__all__ = ['suma', 'potencia', 'factorial']
