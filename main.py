82d2d964a51a8c99f24a8fcb7fcced304a120e6d
Tomasgarciabado <tgarciabado@mail.austral.edu.ar>
2025-05-21 10:04:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ def menu_principal():
         elif opcion == 3:
             num = int(input("Ingrese número entero: "))
             print(f"Factorial: {factorial(num)}")
+
+        elif opcion == 4:
+            a = float(input("Ingresar un numero: "))
+            b = float(input("Ingresar otro numero: "))
+            print(f'Resultado de la resta: {a - b}')
             
     except ValueError:
         print("Error: Ingrese valores numéricos válidos")
