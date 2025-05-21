ae92ac4e0087edeb1ddf5511f36b04eaf054dd44
Lautinet11 <Lautinet11@gmail.com>
2025-05-21 10:17:27 -0300

    Arreglado

--- main.py
+++ main.py
@@ -7,23 +7,16 @@ def menu_principal():
     print("1. Operaciones básicas")
     print("2. Operaciones avanzadas")
     print("3. Operaciones especiales")
-    print("4. Salir")
+    print("4. Resta")
+    print("5. Salir")
 
     try:
         opcion = int(input("Seleccione una opción: "))
         
         if opcion == 1:
-            print("Suma 1, resta 2") 
-            var = int(input("Queres hacer suma o resta ?: "))
-            if var == 1:
                 a = float(input("Ingrese primer número: "))
                 b = float(input("Ingrese segundo número: "))
                 print(f"Resultado suma: {suma(a, b)}")
-            else:
-                a = float(input("Ingrese primer numero : "))
-                b = float(input("Ingrese segundo numero ; "))
-                c = a - b
-                print(f"Resultado de la resta es igual a: {c}")
         elif opcion == 2:
             base = float(input("Ingrese base: "))
             exponente = float(input("Ingrese exponente: "))
@@ -32,7 +25,12 @@ def menu_principal():
         elif opcion == 3:
             num = int(input("Ingrese número entero: "))
             print(f"Factorial: {factorial(num)}")
-            
+        elif opcion == 4:
+            a = float(input("Ingrese primer numero : "))
+            b = float(input("Ingrese segundo numero ; "))
+            c = a - b
+            print(f"Resultado de la resta es igual a: {c}")
+
     except ValueError:
         print("Error: Ingrese valores numéricos válidos")
 
