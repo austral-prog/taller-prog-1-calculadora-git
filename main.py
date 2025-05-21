c2b933d36737bbd689952d9a8a5a45dc236048ef
Lautinet11 <Lautinet11@gmail.com>
2025-05-21 09:39:58 -0300

    Suma y resta

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ def menu_principal():
         opcion = int(input("Seleccione una opción: "))
         
         if opcion == 1:
-            a = float(input("Ingrese primer número: "))
-            b = float(input("Ingrese segundo número: "))
-            print(f"Resultado suma: {suma(a, b)}")
-            
+		print("Suma 1, resta 2") 
+		var = int(input("Queres hacer suma o resta ?: "))
+		if var == 1:
+            		a = float(input("Ingrese primer número: "))
+            		b = float(input("Ingrese segundo número: "))
+           	 	print(f"Resultado suma: {suma(a, b)}")
+		else:
+			a = float(input("Ingrese primer numero : "))
+			b = float(input("Ingrese segundo numero ; "))
+			c = a - b
+			print(f"Resultado de la resta es igual a: {c}")
         elif opcion == 2:
             base = float(input("Ingrese base: "))
             exponente = float(input("Ingrese exponente: "))
